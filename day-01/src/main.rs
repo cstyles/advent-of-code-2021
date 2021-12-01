79206f9d0aa37f5f94bc198ee2d9a01627bba58e
Collin Styles <collingstyles@gmail.com>
2021-11-30 22:15:12 -0800

    Day 01, part 1

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let input: Vec<i32> = include_str!("../input.txt")
+        .lines()
+        .filter_map(|s| s.parse().ok())
+        .collect();
+
+    let part1 = input
+        .windows(2)
+        .filter(|numbers| numbers[0] < numbers[1])
+        .count();
+
+    println!("part1 = {}", part1);
+
+    let part2: Vec<i32> = input
+        .windows(3)
+        .map(IntoIterator::into_iter)
+        .map(Iterator::sum)
+        .collect();
+
+    let part2 = part2.windows(2).filter(|nums| nums[0] < nums[1]).count();
+
+    println!("part2 = {}", part2);
 }
