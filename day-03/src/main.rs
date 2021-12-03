514cc91dec44223b336bf92adbc29837d55e1726
Collin Styles <collingstyles@gmail.com>
2021-12-03 13:36:22 -0800

    Clean up day 03

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,7 +1,7 @@
 #[derive(Debug)]
 enum Bit {
     Zero,
-    One
+    One,
 }
 
 fn main() {
@@ -11,7 +11,7 @@ fn main() {
     let width = input.lines().next().unwrap().len();
 
     let lines: Vec<&str> = input.lines().collect();
-    let number_of_zeroes = most_common_bit(&lines, width);
+    let number_of_zeroes = most_common_bit(&lines);
 
     let mut gamma = 0;
     for (i, bit) in number_of_zeroes.iter().enumerate() {
@@ -23,22 +23,11 @@ fn main() {
     // epsilon is just gamma but with every bit flipped
     let epsilon = gamma ^ 0b0000_1111_1111_1111;
 
-    println!("ugh = {:?}", number_of_zeroes);
-    println!("gamma   = {:12b}", gamma);
-    println!("epsilon = {:12b}", epsilon);
     println!("part1 = {}", gamma * epsilon);
 
     let mut o2_lines = lines.clone();
-    dbg!(&o2_lines);
     for position in 0..width {
-        let number_of_zeroes = most_common_bit(&o2_lines, width);
-
-        dbg!(&number_of_zeroes);
-        match number_of_zeroes[position] {
-            Bit::Zero => println!("keeping 0s"),
-            Bit::One => println!("keeping 1s"),
-        }
-
+        let number_of_zeroes = most_common_bit(&o2_lines);
         match number_of_zeroes[position] {
             Bit::Zero => o2_lines.retain(|num| num.chars().nth(position).unwrap() == '0'),
             Bit::One => o2_lines.retain(|num| num.chars().nth(position).unwrap() == '1'),
@@ -46,18 +35,15 @@ fn main() {
 
         if o2_lines.len() == 1 {
             break;
-        } else {
-            dbg!(&o2_lines);
         }
     }
 
     let oxygen = o2_lines.first().unwrap();
-    dbg!(oxygen);
     let oxygen = u64::from_str_radix(oxygen, 2).unwrap();
 
-    let mut lines = lines.clone();
+    let mut lines = lines;
     for position in 0..width {
-        let number_of_zeroes = most_common_bit(&lines, width);
+        let number_of_zeroes = most_common_bit(&lines);
         match number_of_zeroes[position] {
             Bit::Zero => lines.retain(|num| num.chars().nth(position).unwrap() == '1'),
             Bit::One => lines.retain(|num| num.chars().nth(position).unwrap() == '0'),
@@ -69,15 +55,12 @@ fn main() {
     }
 
     let carbon_dioxide = lines.first().unwrap();
-    dbg!(carbon_dioxide);
     let carbon_dioxide = u64::from_str_radix(carbon_dioxide, 2).unwrap();
-
-    // 8190 = too low
-    dbg!(oxygen);
-    dbg!(carbon_dioxide);
+    println!("part2 = {}", oxygen * carbon_dioxide);
 }
 
-fn most_common_bit(numbers: &[&str], width: usize) -> Vec<Bit> {
+fn most_common_bit(numbers: &[&str]) -> Vec<Bit> {
+    let width = numbers.first().unwrap().len();
     let mut number_of_zeroes = vec![0; width];
 
     for number in numbers {
@@ -88,11 +71,14 @@ fn most_common_bit(numbers: &[&str], width: usize) -> Vec<Bit> {
         }
     }
 
-    number_of_zeroes.into_iter().map(|count| {
-        if count <= numbers.len() / 2 {
-            Bit::One
-        } else {
-            Bit::Zero
-        }
-    }).collect()
+    number_of_zeroes
+        .into_iter()
+        .map(|count| {
+            if count <= numbers.len() / 2 {
+                Bit::One
+            } else {
+                Bit::Zero
+            }
+        })
+        .collect()
 }
