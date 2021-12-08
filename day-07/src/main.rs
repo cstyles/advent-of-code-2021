28bea0bc5f39c10528d362bb02c057fc93bb1167
Collin Styles <collingstyles@gmail.com>
2021-12-08 15:40:41 -0800

    Day 07, part 2

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -11,10 +11,34 @@ fn main() {
     positions.sort();
     let median = positions[positions.len() / 2];
 
-    let fuel: i32 = positions
-        .iter()
-        .map(|position| (position - median).abs())
-        .sum();
-
+    let fuel = total_fuel(median, &positions);
     println!("part1 = {}", fuel);
+
+    let min = positions.first().unwrap();
+    let max = positions.last().unwrap();
+
+    let min_fuel = (*min..=*max)
+        .map(|target| total_fuel_part2(target, &positions))
+        .min()
+        .unwrap();
+    println!("part2 = {}", min_fuel);
+}
+
+fn total_fuel(target: i32, positions: &[i32]) -> i32 {
+    positions
+        .iter()
+        .map(|position| (target - position).abs())
+        .sum()
+}
+
+fn total_fuel_part2(target: i32, positions: &[i32]) -> i32 {
+    positions
+        .iter()
+        .map(|position| part2_fuel(*position, target))
+        .sum()
+}
+
+fn part2_fuel(start: i32, end: i32) -> i32 {
+    let end = (end - start).abs();
+    (end.pow(2) + end) / 2
 }
