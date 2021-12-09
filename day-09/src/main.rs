ac1eb8e6a95c0422ded9e86ef426a3d71e0d3786
Collin Styles <collingstyles@gmail.com>
2021-12-08 21:22:45 -0800

    Day 09: Major refactor

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -3,24 +3,55 @@ use std::collections::HashSet;
 struct Grid(Vec<Vec<u32>>);
 
 impl Grid {
-    fn get(&self, y: isize, x: isize) -> Option<u32> {
-        let y = if y.is_negative() {
-            return None;
-        } else if y as usize >= self.0.len() {
-            return None;
-        } else {
-            y as usize
-        };
+    pub fn new(input: &str) -> Self {
+        let grid = input
+            .lines()
+            .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
+            .collect();
 
-        let x = if x.is_negative() {
-            return None;
-        } else if x as usize >= self.0.first().unwrap().len() {
-            return None;
-        } else {
-            x as usize
-        };
+        Self(grid)
+    }
 
-        Some(self.0[y][x])
+    fn get(&self, y: usize, x: usize) -> u32 {
+        self.0[y][x]
+    }
+
+    fn height(&self) -> usize {
+        self.0.len()
+    }
+
+    fn width(&self) -> usize {
+        self.0.first().unwrap().len()
+    }
+
+    fn neighbor_coords(&self, y: usize, x: usize) -> [Option<(usize, usize)>; 4] {
+        [
+            y.checked_sub(1).map(|y| (y, x)),
+            x.checked_sub(1).map(|x| (y, x)),
+            // y.lt(&(self.height() - 1)).then_some((y + 1, x)) // need nightly for this
+            // x.lt(&(self.width() - 1)).then_some((y, x + 1)) // need nightly for this
+            if y >= self.height() - 1 {
+                None
+            } else {
+                Some((y + 1, x))
+            },
+            if x >= self.width() - 1 {
+                None
+            } else {
+                Some((y, x + 1))
+            },
+        ]
+    }
+
+    fn neighbor_values(&self, y: usize, x: usize) -> impl Iterator<Item = &u32> {
+        self.neighbor_coords(y, x)
+            .into_iter()
+            .flatten()
+            .map(|(y, x)| &self.0[y][x])
+    }
+
+    fn min_neighbor(&self, y: usize, x: usize) -> u32 {
+        *self.neighbor_values(y, x).min().unwrap()
     }
 }
 
@@ -28,87 +59,52 @@ fn main() {
     let input = include_str!("../input.txt");
     // let input = include_str!("../test.txt");
 
-    let grid: Vec<Vec<u32>> = input
-        .lines()
-        .map(|line| line.chars().filter_map(|c| c.to_digit(10)).collect())
-        .collect();
-    let grid = Grid(grid);
-
-    let mut ugh = vec![];
+    let grid = Grid::new(input);
+    let mut low_points = vec![];
     let mut basins = vec![];
+
     for (y, line) in grid.0.iter().enumerate() {
-        let y = y as isize;
-        for (x, number) in line.iter().enumerate() {
-            let x = x as isize;
-
-            let neighbors = [
-                grid.get(y - 1, x - 0),
-                grid.get(y - 0, x - 1),
-                grid.get(y + 1, x - 0),
-                grid.get(y - 0, x + 1),
-            ];
-
-            let min_neighbor = neighbors.into_iter().flatten().min().unwrap();
-            if *number < min_neighbor {
-                ugh.push(*number);
-                basins.push((y as usize, x as usize));
+        for (x, &height) in line.iter().enumerate() {
+            if height < grid.min_neighbor(y, x) {
+                low_points.push(height);
+                basins.push((y, x));
             }
         }
     }
 
-    let part1: u32 = ugh.into_iter().map(|x| x + 1).sum();
+    let part1: u32 = low_points.into_iter().map(|x| x + 1).sum();
     println!("part1 = {}", part1);
-
-    //
-
-    // println!("basis = {:?}", basins);
 
     let mut basin_sizes = vec![];
     for (y, x) in basins {
-        // println!("size = {}", get_basin_size(&grid, y, x));
         basin_sizes.push(get_basin_size(&grid, y, x));
     }
 
-    basin_sizes.sort();
-    let biggets_basins: u32 = basin_sizes.into_iter().rev().take(3).product();
-    println!("part2 = {}", biggets_basins);
+    basin_sizes.sort_unstable();
+    let part2: usize = basin_sizes.into_iter().rev().take(3).product();
+    println!("part2 = {}", part2);
 }
 
-fn get_basin_size(grid: &Grid, y: usize, x: usize) -> u32 {
-    let mut size = 0;
-    let mut explored: HashSet<(usize, usize)> = HashSet::new();
-    let mut to_explore: Vec<(usize, usize)> = vec![(y, x)];
+fn get_basin_size(grid: &Grid, y: usize, x: usize) -> usize {
+    let mut explored = HashSet::new();
+    let mut to_explore = vec![(y, x)];
 
     while let Some((y, x)) = to_explore.pop() {
         if explored.contains(&(y, x)) {
             continue;
         }
 
-        size += 1;
         explored.insert((y, x));
 
-        for neighbor in neighbors(grid, y, x) {
+        for neighbor in grid
+            .neighbor_coords(y, x)
+            .into_iter()
+            .flatten()
+            .filter(|&(y, x)| grid.get(y, x) < 9)
+        {
             to_explore.push(neighbor);
         }
     }
 
-    size
-}
-
-fn neighbors(grid: &Grid, y: usize, x: usize) -> Vec<(usize, usize)> {
-    let y = y as isize;
-    let x = x as isize;
-
-    let neighbors = [(-1, 0), (0, -1), (1, 0), (0, 1)];
-
-    neighbors
-        .into_iter()
-        .map(|(dy, dx)| (y + dy, x + dx))
-        .filter(|&(y, x)| match grid.get(y, x) {
-            None => false,
-            Some(9) => false,
-            _ => true,
-        })
-        .map(|(y, x)| (y as usize, x as usize))
-        .collect()
+    explored.len()
 }
