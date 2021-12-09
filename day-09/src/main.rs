7c51345770f216cbe5cf1e3eba4b2034b2cce4f6
Collin Styles <collingstyles@gmail.com>
2021-12-08 21:38:18 -0800

    Day 09, part 2
    Reminder me to never try racing against time ever again.

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 struct Grid(Vec<Vec<u32>>);
 
 impl Grid {
@@ -33,6 +35,7 @@ fn main() {
     let grid = Grid(grid);
 
     let mut ugh = vec![];
+    let mut basins = vec![];
     for (y, line) in grid.0.iter().enumerate() {
         let y = y as isize;
         for (x, number) in line.iter().enumerate() {
@@ -47,12 +50,65 @@ fn main() {
 
             let min_neighbor = neighbors.into_iter().flatten().min().unwrap();
             if *number < min_neighbor {
-                println!("{}", *number);
                 ugh.push(*number);
+                basins.push((y as usize, x as usize));
             }
         }
     }
 
     let part1: u32 = ugh.into_iter().map(|x| x + 1).sum();
     println!("part1 = {}", part1);
+
+    //
+
+    // println!("basis = {:?}", basins);
+
+    let mut basin_sizes = vec![];
+    for (y, x) in basins {
+        // println!("size = {}", get_basin_size(&grid, y, x));
+        basin_sizes.push(get_basin_size(&grid, y, x));
+    }
+
+    basin_sizes.sort();
+    let biggets_basins: u32 = basin_sizes.into_iter().rev().take(3).product();
+    println!("part2 = {}", biggets_basins);
+}
+
+fn get_basin_size(grid: &Grid, y: usize, x: usize) -> u32 {
+    let mut size = 0;
+    let mut explored: HashSet<(usize, usize)> = HashSet::new();
+    let mut to_explore: Vec<(usize, usize)> = vec![(y, x)];
+
+    while let Some((y, x)) = to_explore.pop() {
+        if explored.contains(&(y, x)) {
+            continue;
+        }
+
+        size += 1;
+        explored.insert((y, x));
+
+        for neighbor in neighbors(grid, y, x) {
+            to_explore.push(neighbor);
+        }
+    }
+
+    size
+}
+
+fn neighbors(grid: &Grid, y: usize, x: usize) -> Vec<(usize, usize)> {
+    let y = y as isize;
+    let x = x as isize;
+
+    let neighbors = [(-1, 0), (0, -1), (1, 0), (0, 1)];
+
+    neighbors
+        .into_iter()
+        .map(|(dy, dx)| (y + dy, x + dx))
+        .filter(|&(y, x)| match grid.get(y, x) {
+            None => false,
+            Some(9) => false,
+            _ => true,
+        })
+        .map(|(y, x)| (y as usize, x as usize))
+        .collect()
 }
