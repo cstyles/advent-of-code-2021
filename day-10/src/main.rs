c88b68df3fd784ac1da663e78359174e639b0bc6
Collin Styles <collingstyles@gmail.com>
2021-12-09 22:48:52 -0800

    Day 10, part 2

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,7 +1,7 @@
 #[derive(Debug)]
 enum Validity {
     Valid,
-    Incomplete,
+    Incomplete(Vec<char>),
     Corrupt(char),
 }
 
@@ -9,26 +9,43 @@ fn main() {
     let input = include_str!("../input.txt");
     // let input = include_str!("../test.txt");
     let mut syntax_error_score = 0;
+    let mut autocomplete_scores: Vec<u64> = vec![];
 
     for line in input.lines() {
         match check_validity(line) {
-            Validity::Valid => continue,
-            Validity::Incomplete => continue,
+            Validity::Valid => unreachable!("no lines are valid"),
+            Validity::Incomplete(stack) => {
+                let score = stack.into_iter().rev().fold(0, |mut acc, elm| {
+                    acc *= 5;
+                    acc += match elm {
+                        '(' => 1,
+                        '[' => 2,
+                        '{' => 3,
+                        '<' => 4,
+                        _ => unreachable!("invalid opener: {}", elm),
+                    };
+                    acc
+                });
+
+                autocomplete_scores.push(score);
+            }
             Validity::Corrupt(illegal_char) => {
-                let score = match illegal_char {
+                syntax_error_score += match illegal_char {
                     ')' => 3,
                     ']' => 57,
                     '}' => 1197,
                     '>' => 25137,
                     _ => unreachable!(),
                 };
-
-                syntax_error_score += score;
             }
         }
     }
 
     println!("part1 = {}", syntax_error_score);
+
+    autocomplete_scores.sort_unstable();
+    let middle = autocomplete_scores[autocomplete_scores.len() / 2];
+    println!("part2 = {:?}", middle);
 }
 
 fn check_validity(line: &str) -> Validity {
@@ -62,6 +79,6 @@ fn check_validity(line: &str) -> Validity {
     if stack.is_empty() {
         Validity::Valid
     } else {
-        Validity::Incomplete
+        Validity::Incomplete(stack)
     }
 }
