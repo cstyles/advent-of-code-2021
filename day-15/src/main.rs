a74fd910b47909a4ef0890aec1941de7b6cf049f
Collin Styles <collingstyles@gmail.com>
2021-12-21 15:10:26 -0800

    15-02 wip
    Turns out the shortest path can go up and left :(
    
    Gonna have to use a more sophisticated algorithm (e.g., Dijkstra's) to
    solve this.

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,21 +1,36 @@
 fn main() {
-    let input = include_str!("../input.txt");
+    let input = include_str!("../test2.txt");
 
-    let grid: Vec<Vec<u32>> = input
+    let mut grid: Vec<Vec<u32>> = input
         .lines()
         .map(str::chars)
         .map(|chars| chars.map(char_to_u32).collect())
         .collect();
 
+    println!("part1 = {}", find_shortest_path(&grid));
+
+    extend_grid(&mut grid);
+
+    for row in &grid {
+        for cell in row {
+            print!("{} ", cell);
+        }
+        println!();
+    }
+
+    // 2822 too high
+    println!("part2 = {}", find_shortest_path(&grid));
+}
+
+fn find_shortest_path(grid: &[Vec<u32>]) -> u32 {
     let rows = grid.len();
     let columns = grid[0].len();
     let last_row = rows - 1;
     let last_column = columns - 1;
 
-    dbg!(rows, columns);
-
-    // TODO: u32?
     let mut dynprog = vec![vec![0u32; columns]; rows];
+
+    // Pre-populate bottom right cell
     dynprog[last_row][last_column] = grid[last_row][last_column];
 
     // Handle last row
@@ -30,6 +45,7 @@ fn main() {
         }
     }
 
+    // Handle everything else
     for (y, row) in grid.iter().enumerate().rev().skip(1) {
         for (x, cell) in row.iter().enumerate().rev().skip(1) {
             dynprog[y][x] = std::cmp::min(dynprog[y][x + 1], dynprog[y + 1][x]) + cell;
@@ -39,16 +55,41 @@ fn main() {
     // Account for the fact that the first cell is never entered)
     dynprog[0][0] -= grid[0][0];
 
-    // for row in &dynprog {
-    //     for cell in row {
-    //         print!("{} ", cell);
-    //     }
-    //     println!();
-    // }
-
-    println!("part1 = {}", dynprog[0][0]);
+    dynprog[0][0]
 }
 
 fn char_to_u32(c: char) -> u32 {
     (c as u32) - ('0' as u32)
+}
+
+fn extend_grid(grid: &mut Vec<Vec<u32>>) {
+    let rows = grid.len();
+    let columns = grid[0].len();
+
+    // Extend rightward
+    for i in 1..5 {
+        for row in grid.iter_mut() {
+            for x in 0..columns {
+                row.push(wrap_add(row[x], i));
+            }
+        }
+    }
+
+    // Extend downward
+    for i in 1..5 {
+        for y in 0..rows {
+            let new_row = grid[y].iter().map(|&x| wrap_add(x, i)).collect();
+            grid.push(new_row);
+        }
+    }
+}
+
+fn wrap_add(x: u32, i: u32) -> u32 {
+    let n = x + i;
+
+    if n > 9 {
+        n - 9
+    } else {
+        n
+    }
 }
