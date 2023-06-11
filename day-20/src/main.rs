6d120d69ed59449b1252c1a7798c61e7d8c1128f
Collin Styles <collingstyles@gmail.com>
2023-06-11 01:25:00 -0700

    Day 20: Get part 1 actually working

--- day-20/src/main.rs
+++ day-20/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     let input = include_str!("../input.txt");
+    // let input = include_str!("../test.txt");
     let mut sections = input.split("\n\n");
 
     let algorithm: Vec<bool> = sections
@@ -9,38 +10,62 @@ fn main() {
         .map(pixel_to_bool)
         .collect();
 
-    let mut image: Vec<Vec<bool>> = sections
+    let image: Vec<Vec<bool>> = sections
         .next()
         .unwrap()
         .lines()
         .map(|line| line.chars().map(pixel_to_bool).collect())
         .collect();
 
-    debug_image(&image);
-    extend_image(&mut image);
+    part1(image.clone(), &algorithm);
+    // part2(image, &algorithm);
+}
 
-    for i in 0..50 {
-        image = apply_algorithm(image, &algorithm, i);
-        debug_image(&image);
-    }
+fn part1(mut image: Vec<Vec<bool>>, algorithm: &[bool]) {
+    extend_image(&mut image, 5, false);
+    // debug_image(&image);
+    image = apply_algorithm(image, algorithm, false);
+    // debug_image(&image);
+    image = apply_algorithm(image, algorithm, true);
+    // debug_image(&image);
 
-    // 5697 too high
-    // 5507 too high
-    // 5469 too high
     println!("part1 = {}", count_pixels(&image));
 }
 
-fn get_index(image: &[Vec<bool>], y: usize, x: usize) -> usize {
+fn part2(mut image: Vec<Vec<bool>>, algorithm: &[bool]) {
+    extend_image(&mut image, 50, false);
+    // debug_image(&image);
+
+    // for i in 0..50 {
+    for i in 0..2 {
+        image = apply_algorithm(image, algorithm, todo!());
+        // extend_image(&mut image, 1, false);
+        extend_image(&mut image, 1, i % 2 == 0);
+        // debug_image(&image);
+    }
+    println!("part2 = {}", count_pixels(&image));
+}
+
+fn get_index(image: &[Vec<bool>], y: usize, x: usize, fill: bool) -> usize {
     let mut bit_pos = 8;
     let mut result = 0;
+    let fill_wedge = vec![fill; image.len()];
 
     for dy in -1..=1 {
         for dx in -1..=1 {
-            let y = ((y as isize) + dy) as usize;
-            let x = ((x as isize) + dx) as usize;
+            let y = y.checked_add_signed(dy);
+            let x = x.checked_add_signed(dx);
 
-            let row = &image[y];
-            let n = row[x];
+            let row = match y {
+                Some(y) if y >= image.len() => &fill_wedge,
+                Some(y) => image[y].as_slice(),
+                None => &fill_wedge,
+            };
+            let n = match x {
+                Some(x) if x >= image.len() => fill,
+                Some(x) => row[x],
+                None => fill,
+            };
             let n = usize::from(n) << bit_pos;
             // let n = usize::from(image[y][x]) << bit_pos;
             result |= n;
@@ -67,20 +92,19 @@ fn bool_to_pixel(b: bool) -> char {
     }
 }
 
-fn extend_image(image: &mut Vec<Vec<bool>>) {
-    const N: usize = 70;
+fn extend_image(image: &mut Vec<Vec<bool>>, n: usize, with: bool) {
     let len = image.len();
 
-    let empty_row = vec![false; len + 2 * N];
-    let empty_wedge = vec![false; N];
+    let empty_row = vec![with; len + 2 * n];
+    let empty_wedge = vec![with; n];
 
     // Extend vertically up
-    for i in 0..N {
+    for i in 0..n {
         image.insert(i, empty_row.clone());
     }
 
     // Extend middle rows horizontally
-    for row in image.iter_mut().skip(N).take(len) {
+    for row in image.iter_mut().skip(n).take(len) {
         *row = [&empty_wedge, row, &empty_wedge]
             .into_iter()
             .flatten()
@@ -89,7 +113,7 @@ fn extend_image(image: &mut Vec<Vec<bool>>) {
     }
 
     // Extend vertically down
-    for _ in 0..N - 1 {
+    for _ in 0..n - 1 {
         image.push(empty_row.clone())
     }
     image.push(empty_row);
@@ -105,12 +129,12 @@ fn debug_image(image: &[Vec<bool>]) {
     println!();
 }
 
-fn apply_algorithm(image: Vec<Vec<bool>>, algorithm: &[bool], round: usize) -> Vec<Vec<bool>> {
+fn apply_algorithm(image: Vec<Vec<bool>>, algorithm: &[bool], fill: bool) -> Vec<Vec<bool>> {
     let mut image2 = image.clone();
 
-    for (y, row) in image.iter().enumerate().skip(1).take(image.len() - 2) {
-        for (x, _pixel) in row.iter().enumerate().skip(1).take(row.len() - 2) {
-            let index = get_index(&image, y, x);
+    for (y, row) in image.iter().enumerate() {
+        for (x, _pixel) in row.iter().enumerate() {
+            let index = get_index(&image, y, x, fill);
             image2[y][x] = algorithm[index];
         }
     }
