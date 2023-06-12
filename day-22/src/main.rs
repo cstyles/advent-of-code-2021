1b68604ff02b3ccbcbc024ce295fe79fc3696931
Collin Styles <collingstyles@gmail.com>
2023-06-11 23:24:25 -0700

    Day 22: Remove unused part 1 stuff and rename part 2 stuff

--- day-22/src/main.rs
+++ day-22/src/main.rs
@@ -1,58 +1,20 @@
-use std::ops::RangeInclusive;
-
 #[derive(Debug)]
 struct Step {
-    on: bool,
-    x: RangeInclusive<usize>,
-    y: RangeInclusive<usize>,
-    z: RangeInclusive<usize>,
-}
-
-impl TryFrom<&str> for Step {
-    type Error = ();
-
-    fn try_from(string: &str) -> Result<Self, Self::Error> {
-        let (on, ranges) = string.split_once(' ').unwrap();
-        let on = on == "on";
-
-        let mut ranges = ranges.split(',');
-        let x = parse_range(ranges.next().unwrap()).ok_or(())?;
-        let y = parse_range(ranges.next().unwrap()).ok_or(())?;
-        let z = parse_range(ranges.next().unwrap()).ok_or(())?;
-
-        Ok(Self { on, x, y, z })
-    }
-}
-
-impl Step {
-    /// Returns an iterator over all coordinate triplets in the sub-cube
-    fn coords(&self) -> impl Iterator<Item = (usize, usize, usize)> + '_ {
-        // TODO: Might be responsible for the stack overflow in debug builds
-        self.x
-            .clone()
-            .flat_map(|x| std::iter::repeat(x).zip(self.y.clone()))
-            .flat_map(|xy| std::iter::repeat(xy).zip(self.z.clone()))
-            .map(|((x, y), z)| (x, y, z))
-    }
-}
-
-#[derive(Debug)]
-struct Step2 {
     on: bool,
     xs: (isize, isize),
     ys: (isize, isize),
     zs: (isize, isize),
 }
 
-impl Step2 {
+impl Step {
     fn parse(string: &str) -> Self {
         let (on, ranges) = string.split_once(' ').unwrap();
         let on = on == "on";
 
         let mut ranges = ranges.split(',');
-        let xs = parse_range2(ranges.next().unwrap());
-        let ys = parse_range2(ranges.next().unwrap());
-        let zs = parse_range2(ranges.next().unwrap());
+        let xs = parse_range(ranges.next().unwrap());
+        let ys = parse_range(ranges.next().unwrap());
+        let zs = parse_range(ranges.next().unwrap());
 
         Self { on, xs, ys, zs }
     }
@@ -127,7 +89,7 @@ impl Step2 {
     }
 }
 
-fn parse_range2(string: &str) -> (isize, isize) {
+fn parse_range(string: &str) -> (isize, isize) {
     // Trim leading "x=", etc.
     let string = &string[2..];
 
@@ -138,50 +100,20 @@ fn parse_range2(string: &str) -> (isize, isize) {
     (left, right)
 }
 
-fn parse_range(string: &str) -> Option<RangeInclusive<usize>> {
-    // Trim leading "x=", etc.
-    let string = &string[2..];
-
-    let (left, right) = string.split_once("..").unwrap();
-    let left = left.parse::<isize>().unwrap();
-    let right = right.parse::<isize>().unwrap();
-
-    // If the range is entirely outside the initialization area, exit early
-    if (left < -50 && right < -50) || left > 50 && right > 50 {
-        return None;
-    }
-
-    // We only care about coordinates inside the initialization area
-    let left = left.clamp(-50, 50);
-    let right = right.clamp(-50, 50);
-
-    // Swap left and right so that the range is ascending (and thus not empty)
-    let (left, right) = if left > right {
-        (right, left)
-    } else {
-        (left, right)
-    };
-
-    Some(RangeInclusive::new(
-        (left + 50) as usize,
-        (right + 50) as usize,
-    ))
-}
-
 fn main() {
     let input = include_str!("../input.txt");
-    let steps = input.lines().map(Step2::parse);
-    let part1_steps = steps.clone().filter_map(Step2::clamp).collect();
+    let steps = input.lines().map(Step::parse);
+    let part1_steps = steps.clone().filter_map(Step::clamp).collect();
     let part2_steps = steps.collect();
 
     part(1, part1_steps);
     part(2, part2_steps);
 }
 
-fn part(part: u8, steps: Vec<Step2>) {
-    let mut cubes: Vec<Step2> = vec![];
+fn part(part: u8, steps: Vec<Step>) {
+    let mut cubes: Vec<Step> = vec![];
     for step in steps {
-        let to_push: Vec<Step2> = cubes
+        let to_push: Vec<Step> = cubes
             .iter()
             .filter_map(|cube| step.intersect(cube))
             .collect();
